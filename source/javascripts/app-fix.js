a63f593b440d28d3fe29fcb5120e2b7df9966a1a
edahl9 <edahl@pivotal.io>
2016-03-26 11:29:42 -0700

    message center js

---
+++ source/javascripts/app-fix.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+   $("#msg-toggle").on('click', function(){
+        if($("#msg-center").hasClass("msg-show")) {
+            $("#msg-center").removeClass("msg-show");
+            
+        }
+        else {
+            $("#msg-center").addClass("msg-show");
+
+        }
+    });
+
+   $(".message-input").on('click', function(){
+        $(".message-view .error").removeClass("hide");
+
+    });
+
+});
