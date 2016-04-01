d01c022f4cc090c2c9adc6e55917060f0e57c9a5
edahl9 <edahl@pivotal.io>
2016-03-31 17:34:12 -0700

    message popup and name

--- source/javascripts/app-fix.js
+++ source/javascripts/app-fix.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
-   $("#msg-toggle").on('click', function(){
-        if($("#msg-center").hasClass("msg-show")) {
+    $("#msg-toggle").on('click', function() {
+        if ($("#msg-center").hasClass("msg-show")) {
             $("#msg-center").removeClass("msg-show");
-            
-        }
-        else {
+
+        } else {
             $("#msg-center").addClass("msg-show");
 
         }
     });
 
-   $(".message-input").on('click', function(){
+    $(".message-input").on('click', function() {
         $(".message-view .error").removeClass("hide");
 
     });
+    setTimeout(
+        function() {
+            $("#msg-center").addClass("msg-show");
+        }, 5000);
+
+    
+
 
 });
